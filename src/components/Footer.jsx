31edc22894522ce8d282e32759921a73deeb7ce5
AniruddhaGaud <gaudaniruddha@gmail.com>
2023-05-12 12:25:02 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,69 @@
+import {
+  FaFacebook,
+  FaHome,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+  FaTwitter,
+} from "react-icons/fa";
+import "./FooterStyles.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "white", marginRight: "2rem" }} />
+            <div>
+              <p>Nashik, Maharashtra.</p>
+              <p>India.</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "white", marginRight: "2rem" }}
+              />
+              91-9977-081-530
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "white", marginRight: "2rem" }}
+              />
+              gaudaniruddha@gmail.com
+            </h4>
+          </div>
+        </div>
+
+        <div className="right">
+          <h4>About Aniruddha.</h4>
+          <p>
+            This is Aniruddha Gaud. Frontend Developer Trainee at DigiKull. I
+            enjoy Developing new Projects and design challenges
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "white", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "white", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "white", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/WorkCardData.jsx
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.png";
+// import pro2 from "../assets/project2.png";
+// import pro3 from "../assets/project3.png";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Dice Game",
+    text: "hbsjdahjsbjhdbjbsajdhbajsdbjhsbdjhabsdjhbsahbdhsbdhbjhbdjhsbjh",
+    view: "https://dice-game-szht.onrender.com/",
+  },
+  {
+    imgsrc: pro1,
+    title: "Dice Game",
+    text: "hbsjdahjsbjhdbjbsajdhbajsdbjhsbdjhabsdjhbsahbdhsbdhbjhbdjhsbjh",
+    view: "https://dice-game-szht.onrender.com/",
+  },
+  {
+    imgsrc: pro1,
+    title: "Dice Game",
+    text: "hbsjdahjsbjhdbjbsajdhbajsdbjhsbdjhabsdjhbsahbdhsbdhbjhbdjhsbjh",
+    view: "https://dice-game-szht.onrender.com/",
+  },
+];
+
+export default ProjectCardData;
